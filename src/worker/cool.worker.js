2b382e9b0ba8f3eac16d54cf88b47df34fdb4ea2
aibarr <alfredo.barra@usach.cl>
2020-01-20 17:33:57 -0300

    Now it's fixed!!

--- src/worker/cool.worker.js
+++ src/worker/cool.worker.js
@@ -1,7 +1,9 @@
 import JustDoing from '../utils'
 
+// eslint-disable-next-line
 self.addEventListener('message', function (event) {
     JustDoing(event.data)
 
+    // eslint-disable-next-line
     self.postMessage(event.data)
 })
